742230be579d22d2674697a789e9e8e57c3219b1
zuhaibakhtarkhan <zuhaibakhtarkhan@gmail.com>
2026-09-02 01:07:08 +0500

    xyz

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,10 +4,9 @@ module.exports = {
       name: "1apply-web",
       cwd: "./apps/web",
       script: "node_modules/next/dist/bin/next",
-      args: "start --port 3000",
+      args: "start -p 3000",
       env: {
         NODE_ENV: "production",
-        PORT: "3000",
       },
       max_memory_restart: "800M",
     },
